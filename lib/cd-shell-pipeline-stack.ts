198c5aef67368876a6994554938ecb5c49e9945d
Uladzimir_Sychou <uladzimir_sychou@epam.com>
2023-04-24 15:03:49 -0400

    yml

--- lib/cd-shell-pipeline-stack.ts
+++ lib/cd-shell-pipeline-stack.ts
@@ -1,4 +1,3 @@
-
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib';
 import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
@@ -7,21 +6,46 @@ import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
-import { DeploymentState } from '../deployment-state/deployment-state';
+import * as yaml from 'js-yaml';
+import * as fs from 'fs';
+import { Deployment } from 'aws-cdk-lib/aws-apigateway';
 
-let deploymentState: DeploymentState = require('../deployment-state/deployment-state.json');
+
+interface DeploymentState {
+  dev: Env;
+  qa: Env;
+  stage: Env;
+  prod: Env;
+}
+
+interface Env {
+  lambdas?: LambdaFunction[]
+}
+
+interface LambdaFunction {
+  name: string;
+  artifact: string;
+  entry: string;
+}
 
 export class CdPipelineStack extends cdk.Stack {
+
+  readDeploymentState(): DeploymentState {
+    const fileContents = fs.readFileSync('deployment-state/deployment-state.yml', 'utf8');
+    return (yaml.load(fileContents) as DeploymentState);
+  }
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
 
-    console.log(deploymentState);
+    const deploymentState: DeploymentState = this.readDeploymentState();
+    console.log(JSON.stringify(deploymentState));
     // Define the S3 bucket where the Lambda function zip file is located
     const bucket = s3.Bucket.fromBucketArn(this, 'ArtifactBucket', 'arn:aws:s3:::artifactory-s3-bucket-1681906290');
 
 
-    const lambdas = deploymentState.envs.dev.lambdaFunctions ? deploymentState.envs.dev.lambdaFunctions : [];
+    const lambdas = deploymentState.dev.lambdas ? deploymentState.dev.lambdas : [];
     const desiredLambda = lambdas[0];
     const lambda_function_artifact_name = desiredLambda.artifact;
 
@@ -103,10 +127,11 @@ export class CdPipelineStack extends cdk.Stack {
           build: {
             commands: [
               'git clone ${REPO_URL}',
-              'cd ${REPO_NAME}',
               'cd ${REPO_NAME}/deployment-state',
               'sudo curl -sL https://github.com/mikefarah/yq/releases/download/v4.13.2/yq_linux_amd64 -o /usr/local/bin/yq && sudo chmod +x /usr/local/bin/yq',
-              'artifact=$(yq r deployment-state.yml dev.lamdas.(name==$FN_NAME).artifact)',
+              'printenv',
+              'yq --version',
+              `artifact=$(yq e '.dev.lambdas[] | select(.name == "GreetingLambda") | .artifact' deployment-state.yml)`,
               'echo $artifact',
               'metadata_url="http://169.254.170.2$(echo $AWS_CONTAINER_CREDENTIALS_RELATIVE_URI)"',
               'creds=$(curl --silent $metadata_url)',
@@ -128,6 +153,19 @@ export class CdPipelineStack extends cdk.Stack {
       },
     });
 
+    deployProject.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: ['*'],
+      actions: ['lambda:UpdateFunctionCode', 'lambda:UpdateFunctionConfiguration'],
+    }));
+
+    deployProject.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      resources: [bucket.bucketArn, `${bucket.bucketArn}/*`],
+      actions: ['s3:GetObject'],
+    }));
+
+    bucket.grantRead(deployProject);
 
     const deployAction = new codepipelineActions.CodeBuildAction({
       actionName: 'Deploy',
